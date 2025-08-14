4f1bdadd17c3c036ac1d8aff33195c200ec5535b
labhanshu03 <143610953+labhanshu03@users.noreply.github.com>
2025-08-14 21:08:03 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -7,7 +7,7 @@ import { IoTrophy } from 'react-icons/io5'
 
 export const userDataContext=createContext()
 function UserContext({children}) {
-    const serverUrl="http://localhost:8000"
+    const serverUrl="https://virtualassistant-backend-rh1h.onrender.com"
     const [userData,setUserData]=useState(null)
        const [frontendImage,setFrontendImage]=useState(null)
         const [backendImage,setBackendImage]=useState(null)
