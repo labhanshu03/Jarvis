a03ef6680973a535d64ee0aef8afdbdf47d8d11a
labhanshu03 <143610953+labhanshu03@users.noreply.github.com>
2025-08-14 21:15:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-     origin:"http://localhost:5173",
+     origin:"https://virtualassistant-jqph.onrender.com",
      credentials:true
 }))
 
