0c5935a52439ca7464e05aed7e616bea5b33f561
labhanshu03 <143610953+labhanshu03@users.noreply.github.com>
2025-10-22 02:45:21 +0530

    Change placeholder text in input field

--- frontend/src/pages/Customize2.jsx
+++ frontend/src/pages/Customize2.jsx
@@ -41,7 +41,7 @@ function Customize2() {
  <div className='w-full h-[100vh] bg-gradient-to-t from-[black] to-[#030353] flex justify-center items-center flex-col p-[20px] relative'>
   <IoMdArrowRoundBack onClick={()=>navigate("/customize")} className='cursor-pointer absolute top-[30px] left-[30px] text-white w-[25px] h-[25px]'/>
     <h1 className='text-white mb-[40px] text-[30px] text-center'>Enter your <span className='text-blue-200' >Assistant Name</span></h1>
-    <input type="text" onChange={(e)=>setAssistantName(e.target.value)} value={assistantName}  placeholder='eg. shifra' className='w-full max-w-[600px] h-[60px] outline-none border-2 border-white bg-transparent text-white placeholder-gray-300 px-[20px] py-[10px] rounded-full text-[18px]'/>
+    <input type="text" onChange={(e)=>setAssistantName(e.target.value)} value={assistantName}  placeholder='eg. Jarvis' className='w-full max-w-[600px] h-[60px] outline-none border-2 border-white bg-transparent text-white placeholder-gray-300 px-[20px] py-[10px] rounded-full text-[18px]'/>
     {assistantName && <button className="min-w-[300px] h-[60px] text-black font-semibold bg-white rounded-full text-[19px] mt-[20px] cursor-pointer" disabled={loading} onClick={()=>{handleUpdateAssistant();navigate("/")}}>{!loading?"Finally create your assistant":"Loading..."}</button>}
       </div>
   )
